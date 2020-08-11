0b12fb47ebab03f180f6767063087b2d4e4bc306
Nabeil Lemar <nabeillemar@gmail.com>
2020-08-11 14:22:13 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   new_array = array(element)
-  new_array << "address"
+  new_array << element 
 end
 
 def add_element_to_start_of_array(array, element)
