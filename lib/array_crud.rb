06514c429de9cb5beacf8265c5a5f3f7b5650c81
Nabeil Lemar <nabeillemar@gmail.com>
2020-08-11 14:26:14 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,7 +28,7 @@ end
 
 def retrieve_element_from_index(array, index_number)
   new_array = array
-  new_array = index_number
+  new_array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
